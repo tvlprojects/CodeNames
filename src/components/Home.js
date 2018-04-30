b7c433bf66963a22a0c39821257fe8a11871db54
Lam <tommy.v.lam@accenture.com>
2018-04-30 12:59:20 -0500

    stylized home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import  { Container, Segment, Button } from 'semantic-ui-react';
+import  { Container, Segment, Button, Input, Grid  } from 'semantic-ui-react';
 
 class Home extends Component {
   constructor(props) {
@@ -21,24 +21,40 @@ class Home extends Component {
   }
 
   render() {
+    const submit = <Link to={`/${this.state.input}`}><Button size="massive">Enter</Button></Link>
+
     return (
       <div>
-      <Container>
-        <Segment inverted={true} textAlign="center">
-          <h1>
-            Code Names
-          </h1>
-        </Segment>
-        <div>
-          Enter Game id here:
-          <input
-                type='text'
-                placeholder='Game ID'
-                value={this.state.input}
-                onChange={this.updateInput}
-              />
-            <Link to={`/${this.state.input}`}><Button>Submit</Button></Link>
-          </div>
+        <Container >
+          <Segment inverted={true} textAlign="center">
+            <h1>
+              Code Names
+              </h1>
+          </Segment>
+          <Grid centered={true} padded={"horizontally"}>
+            <Segment size={"small"} compact={true} basic={true}>
+              <Grid.Row>
+                <Container text textAlign={"justified"}>
+                  <h2>
+                    Welcome to Code Names!
+                    This app allows you to play Code Names with your friends online across multiple devices while sharing one board!
+                    Please enter a unique game id below to begin.
+                  </h2>
+                </Container>
+              </Grid.Row>
+              <Grid.Row>
+                <Input
+                  fluid={true}
+                  action={submit}
+                  type='text'
+                  size="massive"
+                  placeholder='Game Id'
+                  value={this.state.input}
+                  onChange={this.updateInput}
+                />
+              </Grid.Row>
+            </Segment>
+          </Grid>
         </Container>
       </div>
     );
